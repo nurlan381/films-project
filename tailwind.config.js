6a9c4529b101f89bf9ffaebede6ff512d009d5d1
nurlan381 <149506250+nurlan381@users.noreply.github.com>
2023-12-02 15:36:04 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+     container: {
+      center: true,
+      screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+      },
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+
+      colors: {
+        'gega-red': '#bc1a45',
+        'gega-melon': '#ffd369',
+        'gega-grey': '#dddddd',
+        'gega-white': '#f7f7f7',
+      },
+
+      spacing:{
+        '128': '32rem',
+        '1140': '1140px',
+      },
+    },
   },
   plugins: [],
 }
